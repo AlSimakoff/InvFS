764ec9826b0071d9992fdac5c5404b5ef156a5d0
Sim <sensei.boi@mail.ru>
2023-06-29 17:00:43 +0300

    Project inventory chill Update itemServiceImpl

--- src/main/java/org/example/controller/TextController.java
+++ src/main/java/org/example/controller/TextController.java
@@ -1,4 +1,7 @@
 package org.example.controller;
+import org.example.repositories.Office;
+import org.example.repositories.Outside;
+import org.example.repositories.Store;
 import org.example.repositories.item;
 import org.example.services.itemServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +15,17 @@ public class TextController {
 
     @Autowired
     private itemServiceImpl ItemService;
-    @GetMapping("/")
+    @GetMapping("/index")
     public String showItems(Model model) {
         List<item> Item = ItemService.getAllItems();
+        List<Office> office = ItemService.getAllOffice();
+        List<Store> store = ItemService.getAllStore();
+        List<Outside> outside = ItemService.getAllOutside();
         model.addAttribute("item", Item);
+        model.addAttribute("office", office);
+        model.addAttribute("store", store);
+        model.addAttribute("outside", outside);
+
         return "index";
     }
 }
--- src/main/java/org/example/services/itemServiceImpl.java
+++ src/main/java/org/example/services/itemServiceImpl.java
@@ -1,6 +1,5 @@
 package org.example.services;
-import org.example.repositories.item;
-import org.example.repositories.itemRepository;
+import org.example.repositories.*;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
@@ -9,23 +8,105 @@ import java.util.Optional;
 
 @Primary
 @Service
-public class itemServiceImpl implements itemService  {
+public class itemServiceImpl implements itemService {
     private final itemRepository ItemRepository;
-    public itemServiceImpl(itemRepository ItemRepository) {
+    private final OfficeRepository officeRepository;
+    private final OutsideRepository outsideRepository;
+    private final StoreRepository storeRepository;
+    private final TransactionRepository transactionRepository;
+
+    public itemServiceImpl(itemRepository ItemRepository, OfficeRepository officeRepository,
+                           OutsideRepository outsideRepository, StoreRepository storeRepository,
+                           TransactionRepository transactionRepository) {
         this.ItemRepository = ItemRepository;
+        this.officeRepository = officeRepository;
+        this.outsideRepository = outsideRepository;
+        this.storeRepository = storeRepository;
+        this.transactionRepository = transactionRepository;
+
     }
 
-    public Optional<item> getitem(int invId){
+    public Optional<item> getitem(int invId) {
         return ItemRepository.getitemById(invId);
     }
 
     public List<item> getAllItems() {
         return ItemRepository.findAll();
     }
-    public void saveitem(item Item){
-    ItemRepository.save(Item);
+
+    public void saveitem(item Item) {
+        ItemRepository.save(Item);
+    }
+
+    ;
+
+    public void deleteitem(int ItemId) {
+        ItemRepository.delete(ItemId);
+    }
+
+    ;
+
+
+    public Optional<Office> getOffice(int invId)
+    {
+        return officeRepository.getById(invId);
+    }
+    public List<Office> getAllOffice() {
+        return officeRepository.findAll();
+    }
+    public void saveOffice(Office Office){
+        officeRepository.save(Office);
     };
-    public void deleteitem(int ItemId){
-    ItemRepository.delete(ItemId);
+    public void deleteOffice(int OfficeId){
+        officeRepository.delete(OfficeId);
+    };
+
+
+
+    public Optional<Outside> getOutside(int invId)
+    {
+        return outsideRepository.getById(invId);
+    }
+    public List<Outside> getAllOutside() {
+        return outsideRepository.findAll();
+    }
+    public void saveOutside(Outside outside){
+        outsideRepository.save(outside);
+    };
+    public void deleteOutside(int invId){
+        outsideRepository.delete(invId);
+    };
+
+
+
+    public Optional<Store> getStore(int invId)
+    {
+        return storeRepository.getById(invId);
+    }
+    public List<Store> getAllStore() {
+        return storeRepository.findAll();
+    }
+    public void saveStore(Store store){
+        storeRepository.save(store);
+    };
+    public void deleteStore(int invId){
+        storeRepository.delete(invId);
+    };
+
+
+    public Optional<Transaction> getTransaction(int invId)
+    {
+        return transactionRepository.getById(invId);
+    }
+    public List<Transaction> getAllTransaction() {
+        return transactionRepository.findAll();
+    }
+    public void saveTransaction(Transaction transaction){
+        transactionRepository.save(transaction);
+    };
+    public void deleteTransaction(int invId){
+        transactionRepository.delete(invId);
     };
 }
+
+
